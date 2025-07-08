8074114a3ce6f56c176306c439875fc5774c0617
muchoki769 <ndungudavidmuchoki@gmail.com>
2025-07-08 16:30:27 +0300

    pnpm

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { pool } = require("./dbConfig");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const flash = require("express-flash");
 // const flash = require("connect-flash");
